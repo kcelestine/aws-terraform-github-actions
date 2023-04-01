f240bc140d7b91ee4b8c52fb14ed76414934923f
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 17:53:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,6 @@ terraform {
   }
 }
 
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           =  "ami-0fb653ca2d3203ac1" 
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
