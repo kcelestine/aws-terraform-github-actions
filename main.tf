a05b4ecf0bc958085ad836796f9ea9ed578956c9
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 19:04:56 -0400

    this mf commit better pass!

--- main.tf
+++ main.tf
@@ -26,4 +26,12 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_vpc" "main2" {
+  cidr_block = "10.1.0.0/16"
+}
 
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
