eca3cd3b56d99a207685d87428ee8199ec725401
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 17:38:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,3 @@
          }
        }
      }
-
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
-
-     resource "aws_instance" "example" {
-       ami           =  "ami-0fb653ca2d3203ac1" 
-       instance_type = "t2.micro"
-     }
