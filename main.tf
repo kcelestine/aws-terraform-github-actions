1628477b0d443d04c08c059b972fdbe23fcb4373
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 18:19:37 -0400

    provider assumes role

--- main.tf
+++ main.tf
@@ -6,12 +6,13 @@ terraform {
       name = "aws-terraform-github-actions"
     }
   }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.61.0"
-    }
+}
+
+provider "aws" {
+  assume_role {
+    role_arn     = "arn:aws:iam::754274460955:role/gh-role"
   }
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
