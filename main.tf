0080eeb264afa774dcc1f70975fc1722f72111a5
Khadijah <khadijah.celestine@gmail.com>
2023-04-01 16:34:07 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+     terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "_cloudcte"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "aws-terraform-github-actions"
+         }
+       }
+     }
+
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
+
